5df2eea479237f3d3d3219ec8201bfa5509a297c
Nirasha <Nirashaatuni69@gmail.com>
2025-03-06 15:20:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Git Tutorial Project
+
+This project contains a simple Python script that adds two numbers.
---
+++ script.py
@@ -0,0 +1,5 @@
+def subtract_numbers(a, b):
+    return a - b
+
+result = subtract_numbers(5, 3)
+print("The difference is:", result)
